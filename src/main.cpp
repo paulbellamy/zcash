c602b06475d4594c8aef8e56f4b80dee58767b33
Wladimir J. van der Laan <laanwj@gmail.com>
2016-04-05 15:14:48 +0200

    Break circular dependency main ↔ txdb
    Break the circular dependency between main and txdb by:
    
    - Moving `CBlockFileInfo` from `main.h` to `chain.h`. I think this makes
      sense, as the other block-file stuff is there too.
    
    - Moving `CDiskTxPos` from `main.h` to `txdb.h`. This type seems
      specific to txdb.
    
    - Pass a functor `insertBlockIndex` to `LoadBlockIndexGuts`. This leaves
      it up to the caller how to insert block indices.
    
    Zcash: This does not actually break the circular dependency for us yet, as we
    still need to pull in bitcoin/bitcoin#7756 and bitcoin/bitcoin#7904.

--- src/main.cpp
+++ src/main.cpp
@@ -3745,7 +3745,7 @@ CBlockIndex * InsertBlockIndex(uint256 hash)
 bool static LoadBlockIndexDB()
 {
     const CChainParams& chainparams = Params();
-    if (!pblocktree->LoadBlockIndexGuts())
+    if (!pblocktree->LoadBlockIndexGuts(InsertBlockIndex))
         return false;
 
     boost::this_thread::interruption_point();
